e3137a94453a838f9eeeb69b5d4a21decb3c0753
Dmitriy Dumanskiy <doom369@gmail.com>
2016-07-28 16:01:58 +0300

    removed creds :)

--- src/main/java/cc/blynk/dt/Launcher.java
+++ src/main/java/cc/blynk/dt/Launcher.java
@@ -12,8 +12,8 @@ public class Launcher {
 
     public static void main(String[] args) throws Exception {
         String hostUrl = "https://dtagdemo.ram.m2m.telekom.com";
-        String user = "pavel@blynk.cc";
-        String pass = "Blynk2016";
+        String user = args[0];
+        String pass = args[1];
         Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new MeasurementReader(hostUrl, user, pass), 0, 5, TimeUnit.SECONDS);
     }
 
